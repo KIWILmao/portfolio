86171d80b6f9aa65219bbcceac70a0d421709d40
DESKTOP-CI67AM7\shubh <shubhamparmar1173@gmail.com>
2024-09-09 20:35:04 +0530

    custom cursor added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
-          {/* <CustomCursor /> */}
+          <CustomCursor />
           {children}
           <Analytics />
           <SpeedInsights />
